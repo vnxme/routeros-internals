451fa3fd726870e26d796c1767f644a790d2dda1
vnxme <46669194+vnxme@users.noreply.github.com>
2025-05-30 21:04:26 +0300

    Vendor 2025-05-30 v69

--- unpack-npk.py
+++ unpack-npk.py
@@ -5,7 +5,9 @@ from MikroTikPatch import NovaPackage, NpkFileContainer, NpkPartID
 def unpack_npk_package(package: NovaPackage, directory: str, skip_files: bool = False, skip_squashfs: bool = False):
     package_name = package[NpkPartID.NAME_INFO].data.name
     if (not skip_squashfs) and (NpkPartID.SQUASHFS in package) and (len(package[NpkPartID.SQUASHFS].data) > 0):
-        open(f'{directory}/{package_name}.sfs', 'wb').write(package[NpkPartID.SQUASHFS].data)
+        filename = f"{directory}/{package_name}.sfs"
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        open(filename, 'wb').write(package[NpkPartID.SQUASHFS].data)
     if (not skip_files) and (NpkPartID.FILE_CONTAINER in package) and (len(package[NpkPartID.FILE_CONTAINER].data) > 0):
         container = NpkFileContainer.unserialize_from(package[NpkPartID.FILE_CONTAINER].data)
         for item in container:
@@ -28,8 +30,11 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='unpack NPK package')
     parser.add_argument('filename', type=str, help='NPK package filename')
-    parser.add_argument('directory', type=str, help='directory to unpack NPK package')
+    parser.add_argument('directory', type=str, help='directory to unpack NPK package',
+                        default=lambda _args: f"{os.path.dirname(_args.filename)}/_{os.path.basename(_args.filename)}"
+                        if _args.filename is not None else None)
     parser.add_argument('--skip-files', action='store_true', help='skip files')
     parser.add_argument('--skip-squashfs', saction='store_true', help='skip squashfs')
     args = parser.parse_args()
-    unpack_npk_file(args.filename, args.directory, args.skip_files, args.skip_squashfs)
+    if args.filename is not None and args.directory is not None and os.path.exists(args.filename):
+        unpack_npk_file(args.filename, args.directory, args.skip_files, args.skip_squashfs)
