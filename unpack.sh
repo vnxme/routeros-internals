dd749f4885b27dca488a3dd778cdc5a9b6c43f8b
vnxme <46669194+vnxme@users.noreply.github.com>
2025-06-24 23:03:50 +0300

    Vendor 2025-06-24 v177

--- unpack.sh
+++ unpack.sh
@@ -279,7 +279,7 @@ function unpack_sfs {
   local RM=false; [ ! -d "${DIR}" ] && mkdir -p "${DIR}" && RM=true
 
   if [ -n "$(unsquashfs -ll "$1")" ]; then
-    unsquashfs -q -d "$2" "$1" || true
+    unsquashfs -q -d "$2" "$1" 2>/dev/null || true
   fi
 
   rsync -rltgoD "${DIR}/" "$2/"
