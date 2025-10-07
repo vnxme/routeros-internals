05aaa6fcb67032db1fbb9fa93be19e73fb6268a8
vnxme <46669194+vnxme@users.noreply.github.com>
2025-10-07 11:48:44 +0300

    Changes v449

--- download.sh
+++ download.sh
@@ -17,27 +17,28 @@
 MD="$(dirname -- "$(realpath -- "$0")")"
 ME="$(basename -- "$0")"
 
-# Define default flag and option values
-DIR=''                              # -d or --dir:      working directory to save files into
-HOST_GITHUB='github.com'            # --host-github:    GitHub hostname
-HOST_VENDOR='download.mikrotik.com' # --host-vendor:    vendor hostname
-IGNORE_BRANCH='false'               # --ignore-branch:  don't download files from a branch
-IGNORE_RELEASE='false'              # --ignore-release: don't download files from a release
-LABEL=''                            # -l or --label:    branch prefix / tag suffix indicating the source of files
-REPO='vnxme/routeros-internals'     # -r or --repo:     GitHub repository
-VERSION=''                          # -v or --version:  software version
+ARG_DIR=''                              # -d or --dir:      working directory to save files into
+ARG_HOST_GITHUB='github.com'            # --host-github:    GitHub hostname, e.g. github.com
+ARG_HOST_VENDOR='download.mikrotik.com' # --host-vendor:    vendor hostname, e.g. download.mikrotik.com
+ARG_IGNORE_BRANCH='false'               # --ignore-branch:  don't download files from a branch
+ARG_IGNORE_RELEASE='false'              # --ignore-release: don't download files from a release
+ARG_LABEL='vendor'                      # -l or --label:    branch prefix / tag suffix, e.g. vendor
+ARG_REPO='vnxme/routeros-internals'     # -r or --repo:     GitHub repository, e.g. vnxme/routeros-internals
+ARG_VERSION=''                          # -v or --version:  software version, e.g. 7.20
+
+FILE_DOWNLOADS='downloads.txt'
+FILE_PACKAGES='packages.txt'
 
 declare -a DOWNLOADS
 
 function check_deps {
   local DEPS=('wget')
   local DEP; for DEP in ${DEPS[@]}; do
-    [ -z "$(which -- "${DEP}")" ] && { echo "${ME}: Dependency '${DEP}' can't be satisfied"; exit 1; }
+    [ -z "$(which -- "${DEP}")" ] && { echo "${ME}: Dependency '${DEP}' can't be satisfied. Exiting"; exit 1; }
   done
 }
 
 function download {
-  # arguments:
   # $1 - local file path
   # $2 - remote file URL
 
@@ -45,83 +46,85 @@ function download {
 }
 
 function download_from_branch {
-  # arguments:
   # $1 - local file path
 
   download "$1" "${URL_GITHUB_B}/$1"
 }
 
 function download_from_branch_or_vendor {
-  # arguments:
   # $1 - local file path
   # $2 - vendor file name
+  # $3 - alternative vendor URL
 
-  if [ "${IGNORE_BRANCH}" == "false" ]; then
+  if [ "${ARG_IGNORE_BRANCH}" == "false" ]; then
     download_from_branch "$1"
   fi
 
   if [ ! -f "$1" ]; then
-    download "$1" "${URL_VENDOR}/$2"
+    ARG_IGNORE_BRANCH='true'
+    download_from_vendor "$1" "$2" "$3"
   fi
 }
 
 function download_from_release {
-  # arguments:
   # $1 - local file path
 
   download "$1" "${URL_GITHUB_R}/$1"
 }
 
 function download_from_vendor {
-  # arguments:
   # $1 - local file path
   # $2 - vendor file name
+  # $3 - alternative vendor URL
 
-  download "$1" "${URL_VENDOR}/$2"
+  if [ -z "$3" ]; then
+    download "$1" "${URL_VENDOR}/$2"
+  else
+    download "$1" "$3/$2"
+  fi
 }
 
-function parse_options {
-  # arguments
+function parse_arguments {
   # $1... positional arguments
 
-  OPTIONS_L='dir:,host-github:,host-vendor:,ignore-release,ignore-branch,label:,repo:,version:'
-  OPTIONS_S='d:l:r:v:'
+  local OPTIONS_L='dir:,host-github:,host-vendor:,ignore-release,ignore-branch,label:,repo:,version:'
+  local OPTIONS_S='d:l:r:v:'
 
-  PARSED=$(getopt -o ${OPTIONS_S} -l ${OPTIONS_L} --name "${ME}" -- "$@") || exit 2
+  local PARSED=$(getopt -o ${OPTIONS_S} -l ${OPTIONS_L} --name "${ME}" -- "$@") || exit 2
   eval set -- "$PARSED"
 
   while true; do
     case "$1" in
       -d|--dir)
-        DIR="$2"
+        ARG_DIR="$2"
         shift 2
         ;;
       --host-github)
-        HOST_GITHUB="$2"
+        ARG_HOST_GITHUB="$2"
         shift 2
         ;;
       --host-vendor)
-        HOST_VENDOR="$2"
+        ARG_HOST_VENDOR="$2"
         shift 2
         ;;
       --ignore-branch)
-        IGNORE_BRANCH='true'
+        ARG_IGNORE_BRANCH='true'
         shift
         ;;
       --ignore-release)
-        IGNORE_RELEASE='true'
+        ARG_IGNORE_RELEASE='true'
         shift
         ;;
       -l|--label)
-        LABEL="$2"
+        ARG_LABEL="$2"
         shift 2
         ;;
       -r|--repo)
-        REPO="$2"
+        ARG_REPO="$2"
         shift 2
         ;;
       -v|--version)
-        VERSION="$2"
+        ARG_VERSION="$2"
         shift 2
         ;;
       --)
@@ -135,14 +138,15 @@ function parse_options {
     esac
   done
 
-  [ -z "${DIR}" -a $# -gt 0 ] && DIR="$1" && shift
-  [ -z "${LABEL}" ] && echo "${ME}: Label is required, but missing. Use -l or --label. Exiting" && exit 1
-  [ -z "${VERSION}" ] && echo "${ME}: Version is required, but missing. Use -v or --version. Exiting" && exit 1
-  [ $# -gt 0 ] && echo "${ME}: Got more than one positional argument. Exiting" && exit 1
+  [ -z "${ARG_VERSION}" ] && { echo "${ME}: Version is required, but missing. Use -v or --version. Exiting"; exit 1; }
+  [ -z "${ARG_LABEL}" ] && { echo "${ME}: Label is required, but missing. Use -l or --label. Exiting"; exit 1; }
 
-  if [ -n "${DIR}" ]; then
-    [ ! -d "${DIR}" ] && { echo "${ME}: Not found directory ${DIR} ($(realpath -- "${DIR}")). Exiting"; exit 1; }
-    cd -- "${DIR}"
+  [ -z "${ARG_DIR}" -a $# -gt 0 ] && { ARG_DIR="$1"; shift; }
+  [ $# -gt 0 ] && { echo "${ME}: Got an unknown positional argument. Exiting"; exit 1; }
+
+  if [ -n "${ARG_DIR}" ]; then
+    [ ! -d "${ARG_DIR}" ] && { echo "${ME}: Not found directory ${ARG_DIR} ($(realpath -- "${ARG_DIR}")). Exiting"; exit 1; }
+    cd -- "${ARG_DIR}"
   fi
 }
 
@@ -150,18 +154,17 @@ check_deps
 
 [ $# -gt 0 ] && { echo "${ME}: Started with $# arguments: $@"; } || { echo "${ME}: Started with no arguments"; }
 
-parse_options "$@"
+parse_arguments "$@"
 
-BRANCH="${LABEL}-${VERSION}"
-TAG="v${VERSION}-${LABEL}"
+BRANCH="${ARG_LABEL}-${ARG_VERSION}"
+TAG="v${ARG_VERSION}-${ARG_LABEL}"
 
-URL_GITHUB_R="https://${HOST_GITHUB}/${REPO}/releases/download/${TAG}"
-URL_GITHUB_B="https://${HOST_GITHUB}/${REPO}/raw/refs/heads/${BRANCH}"
-URL_VENDOR="https://${HOST_VENDOR}/routeros/${VERSION}"
+URL_GITHUB_R="https://${ARG_HOST_GITHUB}/${ARG_REPO}/releases/download/${TAG}"
+URL_GITHUB_B="https://${ARG_HOST_GITHUB}/${ARG_REPO}/raw/refs/heads/${BRANCH}"
+URL_VENDOR="https://${ARG_HOST_VENDOR}/routeros/${ARG_VERSION}"
 
 # Download all files from a full release archive
-FILE_DOWNLOADS='downloads.txt'
-if [ "${IGNORE_RELEASE}" == 'false' ]; then
+if [ "${ARG_IGNORE_RELEASE}" == 'false' ]; then
   FILE="routeros-${BRANCH}-full.tar.zst"
   download_from_release "${FILE}"
   if [ -f "${FILE}" ]; then
@@ -179,13 +182,12 @@ fi
 download_from_branch_or_vendor 'changelog.txt' 'CHANGELOG'
 
 # Download a list of extra packages
-FILE_PACKAGES='packages.txt'
 USE_FILE_PACKAGES='false'
-if [ "${IGNORE_BRANCH}" == 'false' ]; then
+if [ "${ARG_IGNORE_BRANCH}" == 'false' ]; then
   download_from_branch "${FILE_PACKAGES}"
   [ -f "${FILE_PACKAGES}" ] && USE_FILE_PACKAGES='true'
 fi
-[ "${USE_FILE_PACKAGES}" == 'false' ] && touch "${FILE_PACKAGES}" && DOWNLOADS+=("${FILE_PACKAGES}")
+[ "${USE_FILE_PACKAGES}" == 'false' ] && { truncate -s 0 -- "${FILE_PACKAGES}"; DOWNLOADS+=("${FILE_PACKAGES}"); }
 
 # Download all packages
 ARCHS=('arm' 'arm64' 'mipsbe' 'mmips' 'ppc' 'smips' 'tile' 'x86')
@@ -195,7 +197,7 @@ for ARCH in "${ARCHS[@]}"; do
   [ "${ARCH}" == 'x86' ] && SUFFIX='' || SUFFIX="-${ARCH}"
 
   for PACKAGE in "${PACKAGES[@]}"; do
-    download_from_branch_or_vendor "${ARCH}/${PACKAGE}-${VERSION}${SUFFIX}.npk" "${PACKAGE}-${VERSION}${SUFFIX}.npk"
+    download_from_branch_or_vendor "${ARCH}/${PACKAGE}-${ARG_VERSION}${SUFFIX}.npk" "${PACKAGE}-${ARG_VERSION}${SUFFIX}.npk"
   done
 
   if [ ${#PACKAGES[@]} -eq 1 -a "${PACKAGES[0]}" == 'routeros' ]; then
@@ -205,8 +207,8 @@ for ARCH in "${ARCHS[@]}"; do
         download_from_branch "${PACKAGE}"
       done
     else
-      FILE="${ARCH}/all_packages-${VERSION}${SUFFIX}.zip"
-      download_from_vendor "${FILE}" "all_packages-${ARCH}-${VERSION}.zip"
+      FILE="${ARCH}/all_packages-${ARG_VERSION}${SUFFIX}.zip"
+      download_from_vendor "${FILE}" "all_packages-${ARCH}-${ARG_VERSION}.zip"
       if [ -f "${FILE}" ]; then
         unset -v 'DOWNLOADS[-1]'
         unzip -o -d "${ARCH}/" "${FILE}" && rm -f "${FILE}"
@@ -225,11 +227,11 @@ for ARCH in "${ARCHS[@]}"; do
   mkdir -p "${ARCH}"
   [ "${ARCH}" == 'x86' ] && SUFFIX='' || SUFFIX="-${ARCH}"
 
-  download_from_branch_or_vendor "${ARCH}/chr-${VERSION}${SUFFIX}.img.zip" "chr-${VERSION}${SUFFIX}.img.zip"
-  download_from_branch_or_vendor "${ARCH}/mikrotik-${VERSION}${SUFFIX}.iso" "mikrotik-${VERSION}${SUFFIX}.iso"
+  download_from_branch_or_vendor "${ARCH}/chr-${ARG_VERSION}${SUFFIX}.img.zip" "chr-${ARG_VERSION}${SUFFIX}.img.zip"
+  download_from_branch_or_vendor "${ARCH}/mikrotik-${ARG_VERSION}${SUFFIX}.iso" "mikrotik-${ARG_VERSION}${SUFFIX}.iso"
 
   if [ "${ARCH}" == 'x86' ]; then
-    download_from_branch_or_vendor "${ARCH}/install-image-${VERSION}${SUFFIX}.img.zip" "install-image-${VERSION}${SUFFIX}.zip"
+    download_from_branch_or_vendor "${ARCH}/install-image-${ARG_VERSION}${SUFFIX}.img.zip" "install-image-${ARG_VERSION}${SUFFIX}.zip"
   fi
 done
 
@@ -239,9 +241,13 @@ for ARCH in "${ARCHS[@]}"; do
   mkdir -p "${ARCH}"
   [ "${ARCH}" == 'x86' ] && SUFFIX='' || SUFFIX="-${ARCH}"
 
-  download_from_branch_or_vendor "${ARCH}/netinstall-cli-${VERSION}${SUFFIX}.tar.gz" "netinstall-${VERSION}${SUFFIX}.tar.gz"
-  download_from_branch_or_vendor "${ARCH}/netinstall-w32-${VERSION}${SUFFIX}.exe.zip" "netinstall-${VERSION}${SUFFIX}.zip"
-  download_from_branch_or_vendor "${ARCH}/netinstall-w64-${VERSION}${SUFFIX}.exe.zip" "netinstall64-${VERSION}${SUFFIX}.zip"
+  download_from_branch_or_vendor "${ARCH}/netinstall-cli-${ARG_VERSION}${SUFFIX}.tar.gz" "netinstall-${ARG_VERSION}${SUFFIX}.tar.gz"
+  download_from_branch_or_vendor "${ARCH}/netinstall-w32-${ARG_VERSION}${SUFFIX}.exe.zip" "netinstall-${ARG_VERSION}${SUFFIX}.zip"
+  download_from_branch_or_vendor "${ARCH}/netinstall-w64-${ARG_VERSION}${SUFFIX}.exe.zip" "netinstall64-${ARG_VERSION}${SUFFIX}.zip"
 done
+
+# Download other files
+download_from_branch_or_vendor 'packages.csv' 'packages.csv'
+download_from_branch_or_vendor 'repo.json' 'repo.json' 'https://apps.mikrotik.com/easyapps'
 
 printf '%s\n' "${DOWNLOADS[@]}" > ${FILE_DOWNLOADS}
